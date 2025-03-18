fd3960b13959f44b6aecacc595a64e76acc8c204
MyUsername <rosanio.c@gmail.com>
2025-03-18 17:11:29 +0100

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
 
   const products = [
@@ -7,18 +9,40 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts);
+
+  const addToCart = product => {
+    const isProductAlreadyAdded = addedProducts.some(p => p.name === product.name);
+
+    if (isProductAlreadyAdded) {
+      return;
+    }
+    setAddedProducts(curr => [...curr, {
+      ...product,
+      quantity: 1,
+    }]);
+  }
+
+
+
+  console.log(addedProducts)
+
   return (
     <>
-      <h1>Lista della spesa</h1>
+      <h1 className="debug">Prodotti tra cui scegliere</h1>
       <ul>
-        {products.map((product, index) =>
-          <li key={index}>
-            {product.name} - {product.price} €
+        {products.map((p, i) => (
+          <li key={i} className="debug">
+            <div>
+              {p.name} - {p.price} €
+            </div>
+            <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
           </li>
-        )};
+        ))}
       </ul>
     </>
   )
-}
+};
 
 export default App
